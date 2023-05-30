0ea5c80dbf664d1b210ee13e374fa2d11de109dc
Tony Nguyen <nhattruong1811@gmail.com>
2023-05-30 20:35:26 +0700

    abc

---
+++ src/SkipFirstRenderEffect.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function SkipFirstRenderEffect() {
+  const [count, setCount] = React.useState(0);
+  const skipFirstRenderRef = React.useRef(true);
+  
+  React.useEffect(() => {
+    if(skipFirstRenderRef.current) {
+      skipFirstRenderRef.current = false;
+      return
+    }
+    console.log('run effect')
+  },  [count])
+  
+  return (
+    <div className="App">
+      <h2>Test02: Skip first render</h2>
+
+      <button type="button" onClick={() => setCount(prevState => prevState + 1)}>Submit</button>
+    </div>
+  );
+}
